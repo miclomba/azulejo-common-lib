631fc7b04c55b5d5d50c875860149463ae285604
Michael Lombardi <miclomba@gmail.com>
2020-06-27 17:25:08 -0500

    Convert Intraprocess and test_intraprocess to CMake

--- X/Intraprocess/config.h
+++ X/Intraprocess/config.h
@@ -1,8 +1,6 @@
 #ifndef intraprocess_config_h
 #define intraprocess_config_h
 
-#include "Config/config_boost.h"
-
 #define INTRAPROCESS_DLL_EXPORT __declspec(dllexport)
 
 #endif // intraprocess_config_h
