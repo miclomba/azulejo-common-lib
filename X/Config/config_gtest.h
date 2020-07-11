bbe04aebc72c4c596d36e7e43d447a1570d740f7
Michael Lombardi <miclomba@gmail.com>
2020-07-11 18:46:16 -0500

    use gtest/gmock available in vcpkg

--- X/Config/config_gtest.h
+++ X/Config/config_gtest.h
@@ -1,7 +1,8 @@
 #ifndef config_config_gtest_h
 #define config_config_gtest_h
 
-#define GTEST_LANG_CXX11 1
+#define GTEST_LANG_CXX11
+#define GTEST_LINKED_AS_SHARED_LIBRARY 1
 
 #if defined(_WIN64) || defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 #define ROOT_FILESYSTEM "C:/"
--- X/start_win_devenv.bat
+++ X/start_win_devenv.bat
@@ -1,7 +1,11 @@
 @echo off
 SET ORIG_PATH=%PATH%
 
-SET BASE_LINK_DIR=C:/Users/miclomba/source/repos/Backend/X/out/build/x64-Debug
+SET REPOS=C:/Users/miclomba/source/repos
+SET DEPENDENCY_LINK_DIR=%REPOS%/vcpkg/installed/x64-windows/debug/lib
+SET DEPENDENCY_MAN_LINK_BIN=%REPOS%/vcpkg/packages/gtest_x64-windows/debug/bin
+SET DEPENDENCY_MAN_LINK_DIR=%REPOS%/vcpkg/packages/gtest_x64-windows/debug/lib/manual-link
+SET BASE_LINK_DIR=%REPOS%/Backend/X/out/build/x64-Debug
 SET DATABASE_ADAPTERS=%BASE_LINK_DIR%/DatabaseAdapters
 SET ENTITIES=%BASE_LINK_DIR%/Entities
 SET ENTITIESMODULE=%BASE_LINK_DIR%/EntitiesModule
@@ -13,7 +17,7 @@ SET RESOURCES=%BASE_LINK_DIR%/Resources
 SET RESOURCESMODULE=%BASE_LINK_DIR%/ResourcesModule
 
 REM Setting the path so that test_*.exe can find the appropriate dlls during test discovery
-SET PATH=%PATH%;%DATABASE_ADAPTERS%;%ENTITIES%;%ENTITIESMODULE%;%EVENTS%;%FILESYSTEM_ADAPTERS%;%INTERPROCESS%;%INTRAPROCESS%;%RESOURCES%;%RESOURCESMODULE%
+SET PATH=%PATH%;%DEPENDENCY_MAN_LINK_BIN%;%DEPENDENCY_MAN_LINK_DIR%;%DEPENDENCY_LINK_DIR%;%DATABASE_ADAPTERS%;%ENTITIES%;%ENTITIESMODULE%;%EVENTS%;%FILESYSTEM_ADAPTERS%;%INTERPROCESS%;%INTRAPROCESS%;%RESOURCES%;%RESOURCESMODULE%
 
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe"
 
