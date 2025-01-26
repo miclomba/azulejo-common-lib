96d5200214d080176f5c2c65f61cc266e444f915
Michael Lombardi <miclomba@gmail.com>
2025-01-26 07:13:39 -0800

    [COMMON-6] Fix gtest discovery issue and reenable test discovery

--- start_unit_tests.bat
+++ start_unit_tests.bat
@@ -1,23 +1,36 @@
-PROJ_DIR=`pwd`
-"PATH": "${env.PATH};${projectDir}/out/build/x64-Debug/vcpkg_installed/x64-windows/debug/bin;${projectDir}/out/build/x64-Debug/vcpkg_installed/x64-windows/debug/lib;${projectDir}/out/build/x64-Debug/vcpkg_installed/x64-windows/debug/lib/manual-link;${projectDir}/out/build/x64-Debug/DatabaseAdapters;${projectDir}/out/build/x64-Debug/Entities;${projectDir}/out/build/x64-Debug/EntitiesModule;${projectDir}/out/build/x64-Debug/Events;${projectDir}/out/build/x64-Debug/FilesystemAdapters;${projectDir}/out/build/x64-Debug/Interprocess;${projectDir}/out/build/x64-Debug/Intraprocess;${projectDir}/out/build/x64-Debug/Resources;${projectDir}/out/build/x64-Debug/ResourcesModule",
+@echo off
+set PROJ_DIR=%CD%
+
+set PATH=%PATH%;%PROJ_DIR%\vcpkg_installed\x64-windows\debug\bin;^
+%PROJ_DIR%\vcpkg_installed\x64-windows\debug\lib;^
+%PROJ_DIR%\vcpkg_installed\x64-windows\debug\lib\manual-link;^
+%PROJ_DIR%\out\build\x64-Debug\DatabaseAdapters;^
+%PROJ_DIR%\out\build\x64-Debug\Entities;^
+%PROJ_DIR%\out\build\x64-Debug\EntitiesModule;^
+%PROJ_DIR%\out\build\x64-Debug\Events;^
+%PROJ_DIR%\out\build\x64-Debug\FilesystemAdapters;^
+%PROJ_DIR%\out\build\x64-Debug\Interprocess;^
+%PROJ_DIR%\out\build\x64-Debug\Intraprocess;^
+%PROJ_DIR%\out\build\x64-Debug\Resources;^
+%PROJ_DIR%\out\build\x64-Debug\ResourcesModule
 
 echo "Running test_database_adapters"
-./out/test_database_adapters/test_database_adapters
+.\out\build\x64-Debug\test_database_adapters\test_database_adapters
 echo
 echo "Running test_entities"
-./out/test_entities/test_entities
+.\out\build\x64-Debug\test_entities\test_entities
 echo
 echo "Running test_events"
-./out/test_events/test_events
+.\out\build\x64-Debug\test_events\test_events
 echo
 echo "Running test_filesystem_adapters"
-./out/test_filesystem_adapters/test_filesystem_adapters
+.\out\build\x64-Debug\test_filesystem_adapters\test_filesystem_adapters
 echo
 echo "Running test_resources"
-./out/test_resources/test_resources
-# TODO: cpprest sdk is not portable between windows and mac - replace with Drogon
-#echo "Running test_interprocess"
-#./out/test_interprocess/test_interprocess
+.\out\build\x64-Debug\test_resources\test_resources
+echo
+echo "Running test_interprocess"
+.\out\build\x64-Debug\test_interprocess\test_interprocess
 echo
 echo "Running test_intraprocess"
-./out/test_intraprocess/test_intraprocess
+.\out\build\x64-Debug\test_intraprocess\test_intraprocess
